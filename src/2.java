d858e35ad2c1afc3668e8ca62ad966ed48590517
kyungs0ahuron <kyungs0ahuron@gmx.com>
2025-03-04 19:09:18 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,9 @@
+package Finalproject;
+
+import java.util.Scanner;
+
+public class MyMainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
